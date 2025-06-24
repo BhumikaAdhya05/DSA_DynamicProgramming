59e5c8a2d21f50aa0d6ba81999580678bbf8c4bf
BhumikaAdhya05 <bhumikaadhya@gmail.com>
2025-06-24 10:16:56 +0530

    Update 30_cherry_pickup_i_ii.java

--- 30_cherry_pickup_i_ii.java
+++ 30_cherry_pickup_i_ii.java
@@ -42,3 +42,44 @@ public class CherryPickup {
        Space Complexity: O(n^3)
     */
 }
+
+// Problem 2: Cherry Pickup II (LeetCode 1463)
+// https://leetcode.com/problems/cherry-pickup-ii/
+
+public class CherryPickupII {
+
+    // ----------- DP: 3D Memoization -----------
+    public int cherryPickup(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        Integer[][][] dp = new Integer[m][n][n];
+
+        return helper(grid, 0, 0, n - 1, dp);
+    }
+
+    private int helper(int[][] grid, int row, int col1, int col2, Integer[][][] dp) {
+        int m = grid.length, n = grid[0].length;
+
+        if (col1 < 0 || col1 >= n || col2 < 0 || col2 >= n) return 0;
+        if (dp[row][col1][col2] != null) return dp[row][col1][col2];
+
+        int result = grid[row][col1];
+        if (col1 != col2) result += grid[row][col2];
+
+        if (row != m - 1) {
+            int max = 0;
+            for (int d1 = -1; d1 <= 1; d1++) {
+                for (int d2 = -1; d2 <= 1; d2++) {
+                    max = Math.max(max, helper(grid, row + 1, col1 + d1, col2 + d2, dp));
+                }
+            }
+            result += max;
+        }
+
+        return dp[row][col1][col2] = result;
+    }
+
+    /*
+       Time Complexity: O(m * n * n)
+       Space Complexity: O(m * n * n)
+    */
+}
