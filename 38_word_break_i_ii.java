cb04a0f24474337e81f695614755c0f0a8ce5a59
BhumikaAdhya05 <bhumikaadhya@gmail.com>
2025-06-24 10:42:58 +0530

    Update 38_word_break_i_ii.java

--- 38_word_break_i_ii.java
+++ 38_word_break_i_ii.java
@@ -28,3 +28,40 @@ public class WordBreakI {
        Space Complexity: O(n)
     */
 }
+
+// Problem 2: Word Break II
+// LeetCode 140: https://leetcode.com/problems/word-break-ii/
+
+public class WordBreakII {
+
+    Map<String, List<String>> memo = new HashMap<>();
+
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        return dfs(s, wordSet);
+    }
+
+    private List<String> dfs(String s, Set<String> dict) {
+        if (memo.containsKey(s)) return memo.get(s);
+        List<String> result = new ArrayList<>();
+
+        if (dict.contains(s)) result.add(s);
+
+        for (int i = 1; i < s.length(); i++) {
+            String right = s.substring(i);
+            if (!dict.contains(right)) continue;
+
+            List<String> leftParts = dfs(s.substring(0, i), dict);
+            for (String left : leftParts)
+                result.add(left + " " + right);
+        }
+
+        memo.put(s, result);
+        return result;
+    }
+
+    /*
+       Time Complexity: Exponential in worst-case
+       Space Complexity: O(n * k), where k = number of valid partitions
+    */
+}
