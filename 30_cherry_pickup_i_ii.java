9c4565c031380dadda57c0de77b92fc6576d4166
BhumikaAdhya05 <bhumikaadhya@gmail.com>
2025-06-24 10:16:32 +0530

    Create 30_cherry_pickup_i_ii.java

---
+++ 30_cherry_pickup_i_ii.java
@@ -0,0 +1,44 @@
+// Problem 1: Cherry Pickup I (LeetCode 741)
+// https://leetcode.com/problems/cherry-pickup/
+
+public class CherryPickup {
+
+    // ----------- Brute Force Recursion (TLE) -----------
+    public int cherryPickupBrute(int[][] grid) {
+        int n = grid.length;
+        return Math.max(0, dfs(grid, 0, 0, 0, new int[n][n][n]));
+    }
+
+    private int dfs(int[][] grid, int r1, int c1, int r2, int[][][] memo) {
+        int c2 = r1 + c1 - r2;
+
+        if (r1 >= grid.length || c1 >= grid[0].length ||
+            r2 >= grid.length || c2 >= grid[0].length ||
+            grid[r1][c1] == -1 || grid[r2][c2] == -1)
+            return Integer.MIN_VALUE;
+
+        if (r1 == grid.length - 1 && c1 == grid[0].length - 1)
+            return grid[r1][c1];
+
+        if (memo[r1][c1][r2] != 0) return memo[r1][c1][r2];
+
+        int cherries = grid[r1][c1];
+        if (r1 != r2 || c1 != c2) cherries += grid[r2][c2];
+
+        int best = Math.max(
+            Math.max(dfs(grid, r1 + 1, c1, r2 + 1, memo),
+                     dfs(grid, r1, c1 + 1, r2, memo)),
+            Math.max(dfs(grid, r1 + 1, c1, r2, memo),
+                     dfs(grid, r1, c1 + 1, r2 + 1, memo))
+        );
+
+        cherries += best;
+        memo[r1][c1][r2] = cherries;
+        return cherries;
+    }
+
+    /*
+       Time Complexity: O(n^3)
+       Space Complexity: O(n^3)
+    */
+}
